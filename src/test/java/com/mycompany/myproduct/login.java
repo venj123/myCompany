62a4946173b7ac91fa4f788174f2ff0888b5b48c
venkatj <venkatj@collab.net>
2014-09-23 17:53:54 +0530

    modified the case to go to hotmail

--- src/test/java/com/mycompany/myproduct/login.java
+++ src/test/java/com/mycompany/myproduct/login.java
@@ -28,19 +28,14 @@ public class login
 		System.out.println("login working");
 
 		driver = new FirefoxDriver();
- 	        baseUrl = "https://trialzone.collab.net/";
+ 	        baseUrl = "http://www.hotmail.com";
 	        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	        driver.get(baseUrl + "/sf/sfmain/do/home");
-		driver.findElement(By.id("username")).clear();
-		driver.findElement(By.id("username")).sendKeys("tluser");
-		driver.findElement(By.id("pwd")).clear();
-		driver.findElement(By.id("pwd")).sendKeys("Collabnet1!");
-		driver.findElement(By.cssSelector("strong")).click();
+	        driver.get(baseUrl);
 		
 
 		try {
 			assertTrue(driver.findElement(By.cssSelector("BODY")).getText()
-					.matches("^[\\s\\S]*My Workspacedzfx[\\s\\S]*$"));
+					.matches("^[\\s\\S]*Keep me signed inzfx[\\s\\S]*$"));
 
 			System.out.println("Login test passed");
 			Assert.assertTrue( System.currentTimeMillis() > 0 );
@@ -49,7 +44,7 @@ public class login
 			System.out.println("Login test failed");
 			
 			//Assert.assertTrue(false);
-			//e.getMessage();
+			e.printStackTrace();
 			
 		}
 		
