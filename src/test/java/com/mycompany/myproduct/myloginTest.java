5d7b8c20a2f9cc79ac077347869876e8509f835e
venkatj <venkatj@collab.net>
2014-09-25 23:03:57 +0530

    add assert to find element

--- src/test/java/com/mycompany/myproduct/myloginTest.java
+++ src/test/java/com/mycompany/myproduct/myloginTest.java
@@ -22,11 +22,12 @@ public class myloginTest{
                 driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 
         try {
-			//assertTrue(driver.findElement(By.cssSelector("BODY")).getText()
-			driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Keep me signed inaaaaaaaaaaa[\\s\\S]*$");
+			Assert.assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Keep me signed inaaaaaaaaaaa[\\s\\S]*$"));
 
-			//System.err.println("Login test passed");
-			Assert.assertTrue( System.currentTimeMillis() > 0 );
+			//driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Keep me signed inaaaaaaaaaaa[\\s\\S]*$");
+
+			System.err.println("Login test passed");
+			//Assert.assertTrue( System.currentTimeMillis() > 0 );
 
 		} catch (Error e) {
 			e.printStackTrace();
