2560830d7cfb3cf8bd2a4cc724584a0c27285a70
venkatj <venkatj@collab.net>
2014-09-25 19:13:09 +0530

    renamed main to a method

--- src/test/java/com/mycompany/myproduct/myloginTest.java
+++ src/test/java/com/mycompany/myproduct/myloginTest.java
@@ -13,7 +13,7 @@ import org.testng.annotations.Test;
 public class myloginTest{
 
 	@Test
-	public static void main(String[] args) {
+	public void myloginRun() {
 
 		WebDriver driver=new FirefoxDriver();
 	
